d0d91e2eb3fd34d0c5f205843e7d0e48fb5f9b6c
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-05-17 21:42:47 +0300

    add migrations, uuid into uav model

--- uavs/models.py
+++ uavs/models.py
@@ -1,7 +1,10 @@
+from uuid import uuid4
+
 from django.db import models
 
 
 class UAV(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     brand = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     weight = models.FloatField()

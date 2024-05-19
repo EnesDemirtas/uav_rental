661022c58cfdf6c10013004b34cc5b9a01ff53be
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-05-20 01:26:56 +0300

    add OpenAPI support, endpoints and components can be found in http://localhost:8000/api/schema.json

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, include
+from rest_framework.renderers import JSONOpenAPIRenderer
+from rest_framework.schemas import get_schema_view
 
 from api import views
+
+schema_view = get_schema_view(
+    title="UAV Rental API",
+    url="http://localhost:8000/",
+    renderer_classes=[JSONOpenAPIRenderer],
+)
 
 urlpatterns = [
     path('', include('users.urls')),
     path('', include('uavs.urls')),
     path('', include('rentals.urls')),
+    path('schema.json', schema_view),
 
     # Get endpoints to load pages.
     path('register/form', views.register),
